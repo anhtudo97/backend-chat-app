a01c1acaf02fcf829c1bf542fe56b5eb0803d312
anhtudo97 <anhtudo97@gmail.com>
2021-05-02 17:15:51 +0700

    feat: service locator

---
+++ src/service-locator/types.ts
@@ -0,0 +1,21 @@
+const TYPES = {
+  ToolBox: Symbol("toolbox"),
+  GoogleAPI: Symbol("GoogleAPI"),
+  signer: Symbol("signer"),
+  Tokens: Symbol("Tokens"),
+  PrismaCLient: Symbol("PrismaCLient"),
+  Axios: Symbol("Axios"),
+  Storage: Symbol("Storage"),
+  AuthDataSource: Symbol("AuthDataSource"),
+  UserDataSource: Symbol("UserDataSource"),
+  FriendDataSource: Symbol("FriendDataSource"),
+  BadgeDataSource: Symbol("BadgeDataSource"),
+  BlockDataSource: Symbol("BlockDataSource"),
+  NotificationDataSource: Symbol("NotificationDataSource"),
+  ChatDataSource: Symbol("ChatDataSource"),
+  DataSources: Symbol("DataSources"),
+  Validators: Symbol("Validators"),
+  Utils: Symbol("Utils"),
+};
+
+export default TYPES;
