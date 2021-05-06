50e93804a6c31bad08b4c82961d358d1cd896da3
anhtudo97 <anhtudo97@gmail.com>
2021-05-06 22:05:42 +0700

    chore: update run file

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import createApp from "./app";
+import container, { initContainer } from "./service-locator/container";
+import TYPES from "./service-locator/types";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const bootstrap = async () => {
+  await initContainer();
+  const [server, app] = await createApp(container.get(TYPES.ToolBox));
+  const port = Number(process.env.PORT || 4000);
+  const hostname = "localhost";
+  const expressServer = app.listen(port, hostname, () => {
+    console.log(`
+    🚀 Server is up and running.
+    🎉 Listening on port ${port}.
+  `);
+  });
+  server.installSubscriptionHandlers(expressServer);
+};
+
+void bootstrap();
