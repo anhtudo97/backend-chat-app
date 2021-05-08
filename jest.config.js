beeb50a3d008becad3bfc767b65f8276b5e18879
anhtudo97 <anhtudo97@gmail.com>
2021-05-08 14:50:19 +0700

    chore: add config jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["./jest.setup.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/features/**/*", "src/shared/graphql/**/*"],
+};
